9b6fb84859ca28ab8edcfb1e454223effe41c216
Usama Farooq <usama.farooq@centric.ae>
2019-07-08 18:03:22 +0500

    popop modal

--- www/js/controller/list.js
+++ www/js/controller/list.js
@@ -1,3 +1,29 @@
-app.controller('listCtrl', function($scope, $http, $ionicPopup, $state, $ionicHistory) {
+app.controller('listCtrl', function($scope, $http, $ionicPopup, $state, $ionicHistory, $ionicModal) {
+	$scope.modal = $ionicModal.fromTemplateUrl( 'templates/modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+   }).then(function(modal) { $scope.modal = modal; })
 
+   $scope.openModal = function() {
+      $scope.modal.show();
+   };
+	
+   $scope.closeModal = function() {
+      $scope.modal.hide();
+   };
+	
+   //Cleanup the modal when we're done with it!
+   $scope.$on('$destroy', function() {
+      $scope.modal.remove();
+   });
+	
+   // Execute action on hide modal
+   $scope.$on('modal.hidden', function() {
+      // Execute action
+   });
+	
+   // Execute action on remove modal
+   $scope.$on('modal.removed', function() {
+      // Execute action
+   });
 });
