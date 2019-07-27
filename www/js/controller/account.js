b8ec3533cee6e7f8dac8c6e78fd8be666c04ec79
m0zee <moiz.hanif786@gmail.com>
2019-07-27 06:50:05 +0500

    update

--- www/js/controller/account.js
+++ www/js/controller/account.js
@@ -1,3 +1,33 @@
-app.controller('accountCtrl', function($scope, $http, $ionicPopup, $state, $ionicHistory) {
+app.controller('accountCtrl', function($scope, $http, $ionicPopup, $state, $window, $ionicHistory) {
+	$scope.profile = {};
+	$scope.media = media;
 
+	$scope.getProfile = function() {
+		$http({
+	        method: 'GET',
+	        url: api + "user/profile",
+
+	        // data: loginform,
+	        params :{
+	        	user_key : $window.localStorage["user_key"]
+	        } ,
+	        headers: {
+	            'Content-Type': 'application/x-www-form-urlencoded'
+	        }
+	    }).then(function(data, status, headers, config) {
+	    		response = data.data;
+	    		console.log(response);
+	    		$scope.profile = response.data;
+	    	// if (response.status == 200) {
+		    // 	$window.localStorage["user_key"] = response.data
+		    //     $state.go('home.account');
+	    	// }
+	    	// else{
+	    	// 	$ionicPopup.alert({
+			   //    	title: 'login',
+			   //    	template: response.message
+			   //  });
+	    	// }
+	    });
+	}
 });
--- www/js/controller/login.js
+++ www/js/controller/login.js
@@ -1,3 +1,49 @@
-app.controller('loginCtrl', function($scope, $http, $ionicPopup, $state, $ionicHistory) {
+app.controller('loginCtrl', function($scope, $http, $ionicPopup, $state, $window, $ionicHistory) {
+	lc = {};
+	$scope.loginform = {email : '', password : ''};
 
+
+	$scope.loginsubmit = function() {
+		removeMessage();
+		loginform = $scope.loginform;
+		if (loginform.password == '') $scope.errorMessage = 'Please Enter password';
+		if (loginform.email == '') $scope.errorMessage = 'Please enter Email';
+		if ($scope.errorMessage != '') {
+			$ionicPopup.alert({
+			      	title: 'login',
+			      	template: $scope.errorMessage
+			    });
+			return;
+		}
+
+
+		$http({
+	        method: 'POST',
+	        url: api + "authentication/auth_login",
+
+	        // data: loginform,
+	        data: $.param(
+	        	loginform
+	        ),
+	        headers: {
+	            'Content-Type': 'application/x-www-form-urlencoded'
+	        }
+	    }).then(function(data, status, headers, config) {
+	    		response = data.data;
+	    	if (response.status == 200) {
+		    	$window.localStorage["user_key"] = response.data
+		        $state.go('home.account');
+	    	}
+	    	else{
+	    		$ionicPopup.alert({
+			      	title: 'login',
+			      	template: response.message
+			    });
+	    	}
+	    });
+	}
+
+	function removeMessage(){
+		$scope.errorMessage = '';
+	}
 });
